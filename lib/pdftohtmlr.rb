4723b7f0846414f4254a78df83e43c42cca3a3f1
rkrathwohl <rkrathwohl@charitybuzz.com>
2023-07-05 14:15:16 -0400

    Update ruby syntax and add options

--- lib/pdftohtmlr.rb
+++ lib/pdftohtmlr.rb
@@ -9,6 +9,8 @@
 # Copyright:: Copyright (c) 2009 Kit Plummer
 # License:: MIT
 
+# https://www.xpdfreader.com/pdftohtml-man.html
+
 require 'rubygems'
 require 'nokogiri'
 require 'uri'
@@ -16,91 +18,85 @@ require 'open-uri'
 require 'tempfile'
 
 module PDFToHTMLR
-  
-  # Simple local error abstraction
   class PDFToHTMLRError < RuntimeError; end
-  
-  VERSION = '0.4.2'
+
+  VERSION = '0.5.0'
 
   # Provides facilities for converting PDFs to HTML from Ruby code.
   class PdfFile
-    attr :path
-    attr :target
-    attr :user_pwd
-    attr :owner_pwd
-    attr :format
-    
-    def initialize(input_path, target_path=nil, user_pwd=nil, owner_pwd=nil)
-      @path = input_path
+    attr :path, :target, :user_pwd, :owner_pwd, :format, :first_page_to_convert, :last_page_to_convert
+
+    def initialize(pdf_path, target_path: nil, user_pwd: nil, owner_pwd: nil, first_page: nil, last_page: nil)
+      @path = pdf_path
       @target = target_path
       @user_pwd = user_pwd
-      @owner_pwd = owner_pwd      
+      @owner_pwd = owner_pwd
+      @last_page_to_convert = last_page
+      @first_page_to_convert = first_page
     end
 
     # Convert the PDF document to HTML.  Returns a string
-    def convert()
-      errors = ""
-      output = ""
-      
-      if @user_pwd 
-        cmd = "pdftohtml -stdout #{@format} -upw #{@user_pwd}" + ' "' + @path + '"'    
-      elsif @owner_pwd 
-        cmd = "pdftohtml -stdout #{@format} -opw #{@owner_pwd}" + ' "' + @path + '"'
-      else
-        cmd = "pdftohtml -stdout #{@format}" + ' "' + @path + '"'
-      end
-      
-      output = `#{cmd} 2>&1`
+    def convert
+      opts = ['-stdout']
 
-      if (output.include?("Error: May not be a PDF file"))
+      opts << @format if @format
+      opts << "-upw #{@user_pwd}" if @user_pwd
+      opts << "-opw #{@owner_pwd}" if @owner_pwd
+      opts << "\"#{@path}\""
+      opts << "\"#{@target}\"" if @target
+
+      output = `pdftohtml #{opts.join(" ")} 2>&1`
+
+      if output.include?("Error: May not be a PDF file")
         raise PDFToHTMLRError, "Error: May not be a PDF file (continuing anyway)"
-      elsif (output.include?("Error:"))
+      elsif output.include?("Error:")
         raise PDFToHTMLRError, output.split("\n").first.to_s.chomp
       else
-        return output
+        output
       end
     end
-    
+
     # Convert the PDF document to HTML.  Returns a Nokogiri::HTML:Document
-    def convert_to_document() 
-      Nokogiri::HTML.parse(convert())
+    def convert_to_document
+      Nokogiri::HTML.parse(convert)
     end
-    
-    def convert_to_xml()
+
+    def convert_to_xml
       @format = "-xml"
-      convert()
+      convert
     end
-    
-    def convert_to_xml_document()
+
+    def convert_to_xml_document
       @format = "-xml"
-      Nokogiri::XML.parse(convert())
+      Nokogiri::XML.parse(convert)
     end
   end
-  
+
   # Handle a string-based local path as input, extends PdfFile
   class PdfFilePath < PdfFile
-    def initialize(input_path, target_path=nil, user_pwd=nil, owner_pwd=nil)
-      # check to make sure file is legit
-      if (!File.exist?(input_path))
+    def initialize(input_path, target_path: nil, user_pwd: nil, owner_pwd: nil, first_page: nil, last_page: nil)
+      unless File.exist?(input_path)
         raise PDFToHTMLRError, "invalid file path"
       end
-      
-      super(input_path, target_path, user_pwd, owner_pwd)
-      
-    end 
+
+      super(input_path, target_path: target_path, user_pwd: user_pwd, owner_pwd: owner_pwd,
+            first_page: first_page, last_page: last_page)
+    end
   end
-  
+
   # Handle a URI as a remote path to a PDF, extends PdfFile
   class PdfFileUrl < PdfFile
-    def initialize(input_url, target_path=nil, user_pwd=nil, owner_pwd=nil)
-      # check to make sure file is legit
+    def initialize(input_url, target_path: nil, user_pwd: nil, owner_pwd: nil, first_page: nil, last_page: nil)
       begin
-        if ((input_url =~ URI::regexp).nil?)
+        if (input_url =~ URI::regexp).nil?
           raise PDFToHTMLRError, "invalid file url"
         end
+
         tempfile = Tempfile.new('pdftohtmlr')
-        File.open(tempfile.path, 'wb') {|f| f.write(open(input_url).read) }
-        super(tempfile.path, target_path, user_pwd, owner_pwd)
+        File.open(tempfile.path, 'wb') { |f| f.write(open(input_url).read) }
+
+        super(tempfile.path, target_path: target_path, user_pwd: user_pwd, owner_pwd: owner_pwd,
+              first_page: first_page, last_page: last_page)
       rescue => bang
         raise PDFToHTMLRError, bang.to_s
       end
